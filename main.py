f08b72b92ae54110f8f114912628afb398d695ba
Saad <saadaulakh04@gmail.com>
2024-03-17 16:40:52 -0400

    Created initial plot

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import cv2 as cv
 import numpy as np
 import matplotlib as plt
-from tensorflow.python.keras.layers import datasets, layers, models
+from tensorflow.keras import datasets, layers, models
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+
+training_images, testing_images = training_images / 255.0, testing_images / 255.0
+
+class_names = ['Place', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+for i in range(16):
+    plt.subplot(4, 4, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]].encode("utf-8"))
+
+plt.show()
+
+
 
